021c657f3857b9a65478d484d11d1875f4f6da8b
Cortney Gatewood <cortney.gatewood@gmail.com>
2021-08-03 14:15:07 -0400

    imported BootstrapVue (vue/src/main.js), changed favicon icon (vue/public), and heavily edited Login.vue in terms of element's styles (vue/src/views/Login.vue) - CG

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
+import axios from 'axios'
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
-import axios from 'axios'
+
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
