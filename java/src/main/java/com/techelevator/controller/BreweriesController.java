fa3e2f9e3bcb88317c21e7c9c92f19c6deabf15a
Evan Tortola <etortola58@gmail.com>
2021-08-03 17:51:19 -0400

    ET Push 3

--- java/src/main/java/com/techelevator/controller/BreweriesController.java
+++ java/src/main/java/com/techelevator/controller/BreweriesController.java
@@ -3,33 +3,31 @@ package com.techelevator.controller;
 
 import com.techelevator.dao.BreweryDAO;
 import com.techelevator.model.Brewery;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import services.BreweryService;
+import org.springframework.web.bind.annotation.*;
+//import services.BreweryService;
 
 import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/breweries")
 public class BreweriesController {
 
     private BreweryDAO breweryDAO;
-    private BreweryService breweryService;
 
-    public BreweriesController(BreweryDAO breweryDAO, BreweryService breweryService) {
+
+    public BreweriesController(BreweryDAO breweryDAO) {
         this.breweryDAO = breweryDAO;
-        this.breweryService = breweryService;
+
     }
-    // GET API Brewery provides list of all breweries in Cleveland
-    @RequestMapping(path = "/breweries", method = RequestMethod.GET)
+
+    @RequestMapping(path = "", method = RequestMethod.GET)
     public List<Brewery> breweries () {return breweryDAO.list();}
 
 
-    //GET API Brewery by ID provides the brewery with given ID -- int or long for ID???
-    @RequestMapping(path = "/breweries/{id}", method = RequestMethod.GET)
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
     public Brewery getWithId (@Valid
                                @PathVariable int id) {
         return breweryDAO.get(id);
