5080440aec10807796c8a62ce1238371f1fedf9d
Jacob Harris <heatmiser042018@gmail.com>
2021-08-04 17:34:28 -0400

    created model beer class and made a couple changes to breweries controller class

--- java/src/main/java/com/techelevator/controller/BreweriesController.java
+++ java/src/main/java/com/techelevator/controller/BreweriesController.java
@@ -1,15 +1,13 @@
 package com.techelevator.controller;
 import com.techelevator.dao.BreweryDAO;
 import com.techelevator.model.Brewery;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 //import services.BreweryService;
 
 import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class BreweriesController {
 
---
+++ java/src/main/java/com/techelevator/dao/BeerDAO.java
@@ -0,0 +1,29 @@
+package com.techelevator.dao;
+
+public interface BeerDAO {
+
+//    public List<Beer> getAllBeer();
+//
+//    public Beer getBeerByName(String name);
+//
+//    public List<Beer> getBeerByBrewery(Long breweryId);
+//
+//    public Beer getBeerById(Long beerId);
+//
+//    public boolean searchBeerByName(String name);
+
+
+
+
+
+
+
+
+
+
+    //public List<Beer> getAllBeerByBrewery(Long breweryId);  So brewer can update the beer status if the beer is out, or not sold anymore.
+    //public void saveBeer(Beer newBeer)
+    //public void updateActiveBreweryBeers(Long beerId, boolean isActive);
+    //public void RemoveBeer(Long beerId);  accessed by the brewer
+    //public void UpdateBeerDescription(Long beerId, String name, String type, String Description, String imgUrl);
+}
