8a54eea5ade0791a3a20e8fc58ff48ff07697563
Cortney Gatewood <cortney.gatewood@gmail.com>
2021-08-04 19:52:43 -0400

    Beginning work on Review classes and interface (except controller) - CG

---
+++ java/src/main/java/com/techelevator/model/Review.java
@@ -0,0 +1,18 @@
+package com.techelevator.model;
+
+public class Review  {
+    //
+    private Long reviewId;
+    //private Long breweryId;
+        //or is it private int breweryId?
+    //private String breweryName;
+    private String description;
+    private int rating;
+    //private int(?) dateTime;
+
+
+    //
+
+
+    //
+}
