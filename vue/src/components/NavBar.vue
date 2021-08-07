69ddc3186b38561b538bba916718f0d08df5b945
Colten Trimble <colten.trimble19@gmail.com>
2021-08-07 12:50:53 -0400

    Hidden buttons for unauthorized users and ready for beers and reviews

--- vue/src/components/NavBar.vue
+++ vue/src/components/NavBar.vue
@@ -3,7 +3,7 @@
         <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>&nbsp;&nbsp;
       <router-link v-bind:to="{ name: 'login' }" v-if="$store.state.token == ''">Login</router-link>&nbsp;&nbsp;
-      <router-link v-bind:to="{name: 'addBrewery' }" >Add Brewery</router-link>
+      <router-link v-bind:to="{name: 'addBrewery' }" v-if="$store.state.user.authorities[0].name == 'ROLE_ADMIN'" >Add Brewery</router-link>
     </div>
 </template>
 
---
+++ vue/src/views/AddReview.vue
@@ -0,0 +1,18 @@
+<template>
+  <add-review />
+</template>
+
+<script>
+
+import addReview from '@/components/AddReview'
+export default {
+    components: {
+        addReview
+    }
+
+}
+</script>
+
+<style>
+
+</style>
