4507d385464ea514dd19600499ee90bdef5f96c2
Evan Tortola <etortola58@gmail.com>
2021-08-07 12:49:30 -0400

    ET saturday

--- java/src/main/java/com/techelevator/controller/BreweriesController.java
+++ java/src/main/java/com/techelevator/controller/BreweriesController.java
@@ -1,6 +1,7 @@
 package com.techelevator.controller;
 import com.techelevator.dao.BreweryDAO;
 import com.techelevator.model.Brewery;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 // import services.BreweryService;
@@ -48,12 +49,15 @@ public class BreweriesController {
       return breweries;
     }
 
+
     // Brewery by ID provides the brewery with given ID -- int or long for ID???
+
     @RequestMapping(path = "/breweries/{id}", method = RequestMethod.GET)
     public Brewery getWithId (@Valid @PathVariable int id) {
         return breweryDAO.get(id);
     }
 
+    
     @RequestMapping(path = "/breweries/new", method = RequestMethod.POST)
     public void addNewBrewery (@RequestBody Brewery brewery) {
         breweryDAO.addBrewery(brewery.getBreweryName(), brewery.getBreweryStreet(), brewery.getBreweryCity(), brewery.getBreweryState(), brewery.getZipCode(), brewery.getPhoneNumber(), brewery.getHistory(), brewery.getHoursOfOperation(),brewery.getImage());
