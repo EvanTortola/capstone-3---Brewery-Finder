30acbc080870dce349eebf1130b2a5c1ab6f7b72
Colten Trimble <colten.trimble19@gmail.com>
2021-08-08 10:19:49 -0400

    Working on bug fixes in BreweryDetail

--- vue/src/components/BreweryDetail.vue
+++ vue/src/components/BreweryDetail.vue
@@ -18,11 +18,11 @@
                 <img :src="beer.imgUrl" alt="" class="beerImg">
             </div>
         </div>
-         <!-- <router-link :to="{name: 'addBeer', params: {breweryId: brewery.breweryId} }">
+         <router-link :to="{name: 'addBeer', params: {breweryId: brewery.breweryId} }">
             <div v-if="$store.state.user.authorities[0].name == 'ROLE_BREWER'" class="addBeer">
                 <button>Add Beer</button> 
             </div>   
-        </router-link> -->
+        </router-link>
         <router-link :to="{name: 'addReview', params: {breweryId: brewery.breweryId}}">
         <div class="addReview">
             <button>Add Review
