95d58476b0950af9c281d073408813edda1678cf
Cortney Gatewood <cortney.gatewood@gmail.com>
2021-08-09 10:05:46 -0400

    Added welcome text to the BreweryCard component, centered the BreweryForm

--- vue/src/components/BreweryForm.vue
+++ vue/src/components/BreweryForm.vue
@@ -1,33 +1,43 @@
 <template>
-  <form v-on:submit.prevent="submitForm" class="breweryForm">
-      <div class="status-message error" v-show="errorMsg !== ''">{{errorMsg}}</div>
-       <div class="form-group">
-      <label for="breweryName">Name:</label>
-      <input id="breweryName" type="text" class="form-control" v-model="brewery.breweryName" autocomplete="off" />
-    </div>
-     <div class="form-group">
-      <label for="history">History:</label>
-      <input id="history" type="text" class="form-control" v-model="brewery.history">
-      <label for="breweryAddress">Street:</label>
-      <input id="breweryAddress" type="text" class="form-control" v-model="brewery.breweryAddress">   
-      <label for="breweryCity">City:</label>
-      <input id="breweryCity" type="text" class="form-control" v-model="brewery.breweryCity">
-      <label for="breweryState">State:</label>
-      <input id="breweryState" type="text" class="form-control" v-model="brewery.breweryState"> 
-      <label for="zipCode">Zip Code:</label>
-      <input id="zipCode" type="text" class="form-control" v-model="brewery.zipCode">
-      <label for="hoursOfOperation">Hours of Operation:</label>
-      <input id="hoursOfOperation" type="text" class="form-control" v-model="brewery.hoursOfOperation">
-      <label for="phoneNumber">Phone Number:</label>
-      <input id="phoneNumber" type="tel" class="form-control" v-model="brewery.phoneNumber">
-      <label for="image">Image URL:</label>
-      <input id="image" type="text" name="img" accept="image/*" class="form-control" v-model="brewery.image">    
-    </div>
-    <button class="btn btn-submit" v-on:click.prevent="submitForm">Submit</button>
-    <button class="btn btn-cancel" v-on:click.prevent="cancelForm" type="cancel">Cancel</button>
-  </form>
-  
-</template>
+    <div>
+        <b-container fluid class="bv-row">
+                <b-row> 
+                    <b-col></b-col>
+                    <b-col>
+                      <h1>Add Brewery</h1>
+                      <form v-on:submit.prevent="submitForm" class="breweryForm">
+                          <div class="status-message error" v-show="errorMsg !== ''">{{errorMsg}}</div>
+                          <div class="form-group">
+                            <label for="breweryName">Name:</label>
+                            <input id="breweryName" type="text" class="form-control" v-model="brewery.breweryName" autocomplete="off" />
+                          </div>
+                          <div class="form-group">
+                            <label for="history">History:</label>
+                            <input id="history" type="text" class="form-control" v-model="brewery.history">
+                            <label for="breweryAddress">Street:</label>
+                            <input id="breweryAddress" type="text" class="form-control" v-model="brewery.breweryAddress">   
+                            <label for="breweryCity">City:</label>
+                            <input id="breweryCity" type="text" class="form-control" v-model="brewery.breweryCity">
+                            <label for="breweryState">State:</label>
+                            <input id="breweryState" type="text" class="form-control" v-model="brewery.breweryState"> 
+                            <label for="zipCode">Zip Code:</label>
+                            <input id="zipCode" type="text" class="form-control" v-model="brewery.zipCode">
+                            <label for="hoursOfOperation">Hours of Operation:</label>
+                            <input id="hoursOfOperation" type="text" class="form-control" v-model="brewery.hoursOfOperation">
+                            <label for="phoneNumber">Phone Number:</label>
+                            <input id="phoneNumber" type="tel" class="form-control" v-model="brewery.phoneNumber">
+                            <label for="image">Image URL:</label>
+                            <input id="image" type="text" name="img" accept="image/*" class="form-control" v-model="brewery.image">    
+                          </div>
+                          <button class="btn btn-submit" v-on:click.prevent="submitForm">Submit</button>
+                          <button class="btn btn-cancel" v-on:click.prevent="cancelForm" type="cancel">Cancel</button>
+                      </form>
+                    </b-col>
+                    <b-col></b-col>
+              </b-row> 
+          </b-container>
+    </div>  
+  </template>
 
 <script>
 import breweryService from "../services/BreweryService";
